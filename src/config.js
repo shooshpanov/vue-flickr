807539dd277bfbbb3e2e960ce75158d59f5f396b
Sergey S <shooshpanov@yandex.com>
2019-04-05 11:20:12 -0400

    Axios and API call

---
+++ src/config.js
@@ -0,0 +1,3 @@
+export default {
+  api_key: "3556f14a62890e2e22627a112ee302c0"
+};
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,61 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+    <form>
+      <label>
+        Search:
+        <input v-model="tag" type="text" />
+      </label>
+      <button type="submit" class="go-button" @click.prevent="search">
+        Search
+      </button>
+    </form>
+    <p v-if="loading">
+      Loading...
+    </p>
+    <ul v-else>
+      <li v-for="image in images" :key="image.id">{{ image }}</li>
+    </ul>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from "@/components/HelloWorld.vue";
+import config from "@/config";
+import axios from "axios";
 
 export default {
   name: "home",
-  components: {
-    HelloWorld
+  data() {
+    return {
+      loading: false,
+      tag: "",
+      images: []
+    };
+  },
+  methods: {
+    search() {
+      this.loading = true;
+      this.fetchImages().then(response => {
+        this.images = response.data.photos.photo;
+        this.loading = false;
+      });
+    },
+    fetchImages() {
+      return axios({
+        method: "get",
+        url: "https://api.flickr.com/services/rest",
+        params: {
+          method: "flickr.photos.search",
+          api_key: config.api_key,
+          tags: this.tag,
+          extras: "url_n, owner_name, date_taken, views",
+          page: 1,
+          format: "json",
+          nojsoncallback: 1,
+          per_page: 30
+        }
+      });
+    }
   }
 };
 </script>
